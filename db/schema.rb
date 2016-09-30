a3cba947f63aa9accb547baa9e912e8739931095
Rachel <rachel.l.pavilanis@gmail.com>
2016-09-30 10:18:56 -0700

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930165830) do
+ActiveRecord::Schema.define(version: 20160930171540) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20160930165830) do
     t.datetime "completed_at"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.integer  "person_id"
   end
+
+  add_index "tasks", ["person_id"], name: "index_tasks_on_person_id"
 
 end
